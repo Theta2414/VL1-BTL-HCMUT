7652412d4367825f40fb541e5abf18261f896d64
Cosin <cosin.nguyenk25dee@hcmut.edu.vn>
2025-11-02 16:59:28 +0700

    Never let me code again

---
+++ rocket.m
@@ -0,0 +1,46 @@
+clc; clearvars; close all;
+
+g = 9.81;
+
+% hInit = input('hInit: ');
+% mRocket = input('mRocket: ');
+% mFuel = input('mFuel: ');
+% negFuel = input('negFuel: ');
+% vPropulsion = input('vPropulsion: ');
+
+hInit = 0;
+mRocket = 550000;
+mFuel = 500000;
+negFuel = 250;
+vPropulsion = 35000;
+
+tUp = 0:1:(mFuel/negFuel);
+hUp = hInit + vPropulsion .* tUp ...
+            - vPropulsion .* (mRocket / negFuel - tUp) ...
+           .* log(mRocket ./ (mRocket - negFuel .* tUp)) ...
+            - 0.5 * g .* tUp.^2;
+vUp = vPropulsion ...
+   .* log(mRocket ./ (mRocket - negFuel .* tUp)) ...
+    - g .* tUp;
+aUp = (vPropulsion * negFuel) ./ (mRocket - negFuel .* tUp) - g;
+
+skibidi = roots([- 0.5 * g, vUp(end), hUp(end)]);
+skibidi = ceil(skibidi(skibidi > 0));
+
+tDown = (tUp(end) + 1):1:(numel(vUp) + skibidi - 2);
+hDown = hUp(end) + vUp(end) .* [1:1:numel(tDown)] - 0.5 .* g .* [1:1:numel(tDown)] .^ 2;
+hDown = hDown(hDown > 0);
+vDown = vUp(end) - g .* [1:1:numel(tDown)];
+aDown = -g * ones(size(tDown));
+
+figure(1);
+plot([tUp, tDown], [hUp, hDown], 'r');
+grid on;
+
+figure(2);
+plot([tUp, tDown], [vUp, vDown], 'g');
+grid on;
+
+figure(3);
+plot([tUp, tDown], [aUp, aDown], 'b');
+grid on;
