a0c9921df6a8383724570d1cb7719555eb54b643
Cosin <cosin.nguyenk25dee@hcmut.edu.vn>
2025-11-04 21:30:00 +0700

    Fix coeff issue

--- rocket.m
+++ rocket.m
@@ -16,34 +16,35 @@ hUp = hInit + vPropulsion .* tUp ...
             - vPropulsion .* (mRocket / negFuel - tUp) ...
            .* log(mRocket ./ (mRocket - negFuel .* tUp)) ...
             - 0.5 * g .* tUp.^2;
-vUp = vPropulsion ...
-   .* log(mRocket ./ (mRocket - negFuel .* tUp)) ...
-    - g .* tUp;
-aUp = (vPropulsion * negFuel) ./ (mRocket - negFuel .* tUp) - g;
-
-% Nghiem duong cua phuong trinh bac 2 de tim thoi gian roi cua ten lua
-coeff = roots([- 0.5 * g, vUp(end), hUp(end)]);
-coeff = ceil(coeff(coeff > 0));
-
-% % Ket qua tinh duoc khi ten lua het nhien lieu
-tDown = (tUp(end) + 1):1:(numel(vUp) + coeff - 2);
-hDown = hUp(end) + vUp(end) .* (1:1:length(tDown)) - 0.5 .* g .* (1:1:length(tDown)) .^ 2;
-vDown = vUp(end) - g .* (1:1:length(tDown));
-aDown = -g * ones(size(tDown));
-
-% Tong hop cac ket qua
-tTotal = [tUp, tDown];
-hTotal = [hUp, hDown];
-vTotal = [vUp, vDown];
-aTotal = [aUp, aDown];
-
 % Kiem tra ten lua co thuc su bay khong
 if hUp(2) < 0
-    hTotal(:) = 0;
-    vTotal(:) = 0;
-    aTotal(:) = 0;
+    tTotal = tUp;
+    hTotal = zeros(size(tUp));
+    vTotal = zeros(size(tUp));
+    aTotal = zeros(size(tUp));
+else
+    vUp = vPropulsion ...
+       .* log(mRocket ./ (mRocket - negFuel .* tUp)) ...
+        - g .* tUp;
+    aUp = (vPropulsion * negFuel) ./ (mRocket - negFuel .* tUp) - g;
+
+    % Nghiem duong cua phuong trinh bac 2 de tim thoi gian roi cua ten lua
+    coeff = roots([- 0.5 * g, vUp(end), hUp(end)]);
+    coeff = ceil(coeff(coeff > 0));
+
+    % Ket qua tinh duoc khi ten lua het nhien lieu
+    tDown = (tUp(end) + 1):1:(numel(vUp) + coeff - 2);
+    hDown = hUp(end) + vUp(end) .* (1:1:length(tDown)) - 0.5 .* g .* (1:1:length(tDown)) .^ 2;
+    vDown = vUp(end) - g .* (1:1:length(tDown));
+    aDown = -g * ones(size(tDown));
+
+    % Tong hop cac ket qua
+    tTotal = [tUp, tDown];
+    hTotal = [hUp, hDown];
+    vTotal = [vUp, vDown];
+    aTotal = [aUp, aDown];
 end;
-    
+
 % Ve do thi bieu dien ket qua da tinh duoc
 figure(1)
 plot(tTotal, aTotal, 'b');
