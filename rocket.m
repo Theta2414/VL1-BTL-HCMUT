1a118bd1d0ef756eaa6288f15687e5b49370f949
Cosin <cosin.nguyenk25dee@hcmut.edu.vn>
2025-11-04 21:10:16 +0700

    Fix minor issues and improve readability

--- rocket.m
+++ rocket.m
@@ -2,18 +2,15 @@ clc; clearvars; close all;
 
 g = 9.81;
 
-% hInit = input('hInit: ');
-% mRocket = input('mRocket: ');
-% mFuel = input('mFuel: ');
-% negFuel = input('negFuel: ');
-% vPropulsion = input('vPropulsion: ');
+% Các thông s? s? d?ng cho bài toán (LUÔN D??NG!)
+hInit       = input('Do cao ban dau h0 (m): ');
+mRocket     = input('Tong khoi luong ban dau cua ten lua m0 (kg): ');
+% T?ng kh?i l??ng tên l?a luôn l?n h?n kh?i l??ng nhiên li?u lúc ban ??u!
+mFuel       = input('Khoi luong nhien lieu ban dau (kg): ');
+negFuel     = input('Toc do tieu thu nhien lieu dm/dt (kg/s): ');
+vPropulsion = input('Toc do day khi v (m/s): ');
 
-hInit = 0;
-mRocket = 550000;
-mFuel = 500000;
-negFuel = 250;
-vPropulsion = 3500;
-
+% K?t qu? tính ???c khi tên l?a khi còn nhiên li?u
 tUp = 0:1:(mFuel/negFuel);
 hUp = hInit + vPropulsion .* tUp ...
             - vPropulsion .* (mRocket / negFuel - tUp) ...
@@ -24,34 +21,47 @@ vUp = vPropulsion ...
     - g .* tUp;
 aUp = (vPropulsion * negFuel) ./ (mRocket - negFuel .* tUp) - g;
 
+% Nghi?m d??ng c?a ph??ng trình b?c 2 tìm th?i gian r?i c?a tên l?a
 coeff = roots([- 0.5 * g, vUp(end), hUp(end)]);
 coeff = ceil(coeff(coeff > 0));
 
+% K?t qu? tính ???c khi tên l?a khi h?t nhiên li?u
 tDown = (tUp(end) + 1):1:(numel(vUp) + coeff - 2);
-hDown = hUp(end) + vUp(end) .* [1:1:numel(tDown)] - 0.5 .* g .* [1:1:numel(tDown)] .^ 2;
-hDown = hDown(hDown > 0);
-vDown = vUp(end) - g .* [1:1:numel(tDown)];
+hDown = hUp(end) + vUp(end) .* (1:1:length(tDown)) - 0.5 .* g .* (1:1:length(tDown)) .^ 2;
+vDown = vUp(end) - g .* (1:1:length(tDown));
 aDown = -g * ones(size(tDown));
 
+% T?ng h?p các k?t qu?
 tTotal = [tUp, tDown];
 hTotal = [hUp, hDown];
 vTotal = [vUp, vDown];
 aTotal = [aUp, aDown];
 
+% Ki?m tra tên l?a có th?c s? bay không
 if hUp(2) < 0
     hTotal(:) = 0;
     vTotal(:) = 0;
-    aTotal(:) = -g;
+    aTotal(:) = 0;
 end;
     
-figure(1);
-plot(tTotal, hTotal, 'r');
-grid on;
-
-figure(2);
-plot(tTotal, hTotal, 'g');
-grid on;
-
-figure(3);
+% V? ?? th? bi?u di?n k?t qu? ?ã tính ???c
+figure(1)
 plot(tTotal, aTotal, 'b');
+title('GIA TOC THEO THOI GIAN', 'Interpreter', 'latex');
+xlabel('THOI GIAN $(s)$', 'Interpreter', 'latex');
+ylabel('GIA TOC $(m/s^2)$', 'Interpreter', 'latex');
+grid on;
+
+figure(2)
+plot(tTotal, vTotal, 'g');
+title('VAN TOC THEO THOI GIAN', 'Interpreter', 'latex');
+xlabel('THOI GIAN $(s)$', 'Interpreter', 'latex');
+ylabel('VAN TOC $(m/s)$', 'Interpreter', 'latex');
+grid on;
+
+figure(3)
+plot(tTotal, hTotal, 'r');
+title('DO CAO THEO THOI GIAN', 'Interpreter', 'latex');
+xlabel('THOI GIAN $(s)$', 'Interpreter', 'latex');
+ylabel('DO CAO $(m)$', 'Interpreter', 'latex');
 grid on;
